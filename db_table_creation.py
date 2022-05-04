cae8547f42dec9b9661ff81313770d6a856523ad
Soban-Farhan <sobanfarhan@gmail.com>
2022-05-04 16:50:10 -0400

    Fixed issue with table creation where PK fields didn't increment

--- db_table_creation.py
+++ db_table_creation.py
@@ -1,13 +1,13 @@
 from db.database_connection import conn, curr
 
 
-recipe_table = "CREATE TABLE recipes (id serial PRIMARY KEY , " \
+recipe_table = "CREATE TABLE recipes (id INTEGER PRIMARY KEY , " \
                 "name VARCHAR(100), " \
                 "pre_time INTEGER CHECK (pre_time > 0), " \
                 "difficulty INTEGER check (difficulty >= 0 AND difficulty <= 3), " \
                 "vegetarian BOOLEAN)"
 
-recipe_rating = "CREATE TABLE recipe_rating (id serial PRIMARY KEY, " \
+recipe_rating = "CREATE TABLE recipe_rating (id INTEGER PRIMARY KEY, " \
                 "recipe_id INTEGER REFERENCES recipes, " \
                 "rating INTEGER CHECK( rating >= 0 AND rating <= 5))"
 
