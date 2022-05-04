829ae8c1df4c22bf603b41e2d043aef07af088bc
Soban-Farhan <sobanfarhan@gmail.com>
2022-05-04 13:11:30 -0400

    Created mock data from https://www.mockaroo.com/. Generate mock data on db/table create

--- db_table_creation.py
+++ db_table_creation.py
@@ -4,12 +4,12 @@ from db.database_connection import conn, curr
 recipe_table = "CREATE TABLE recipes (id serial PRIMARY KEY , " \
                 "name VARCHAR(100), " \
                 "pre_time INTEGER CHECK (pre_time > 0), " \
-                "difficulty INTEGER check (difficulty > 0 AND difficulty < 5), " \
+                "difficulty INTEGER check (difficulty >= 0 AND difficulty <= 3), " \
                 "vegetarian BOOLEAN)"
 
 recipe_rating = "CREATE TABLE recipe_rating (id serial PRIMARY KEY, " \
                 "recipe_id INTEGER REFERENCES recipes, " \
-                "rating INTEGER CHECK( rating > 0 AND rating < 5))"
+                "rating INTEGER CHECK( rating >= 0 AND rating <= 5))"
 
 
 #  Check if the tables exist. If not, create new recipe table
@@ -18,7 +18,7 @@ if curr.fetchone()[0]:
     pass
 else:
     curr.execute(recipe_table)
-
+    print("Sucess - Created Recipe table.")
 
 
 #  Check if the tables exist. If not, create new recipe rating table
@@ -27,5 +27,13 @@ if curr.fetchone()[0]:
     pass
 else:
     curr.execute(recipe_rating)
+    print("Sucess - Created Recipe rating table.")
+
+    # Both tables were created and now 
+    # add all the mock data to both tables
+    sql_file = open("db/mock_data.sql")
+    sql_as_string = sql_file.read()
+    curr.executescript(sql_as_string)
+    print("Sucess - Generated mock data for Recipe and Rating table.")
 
 conn.commit()
